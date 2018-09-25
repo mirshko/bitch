755c1ee61495319069429f69d4f37a23e7794841
Jeff Reiner <theundeadghost@gmail.com>
2018-09-25 21:23:32 +0200

    :sparkles: Moving to Gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+const IndexPage = () => (
+  <Layout>
+    <a href="https://www.instagram.com/bitch.coffee/">
+      <h1>
+        Bitch <span>Coffee</span>
+      </h1>
+    </a>
+  </Layout>
+);
+
+export default IndexPage;
